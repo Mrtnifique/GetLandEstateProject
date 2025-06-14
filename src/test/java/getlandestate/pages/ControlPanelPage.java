d1c02b77494f823dc23ae589ada3e8660fa71cdf
Mrtnifique <kilincarslanmrt@gmail.com>
2025-06-14 14:10:27 +0300

    Its alive! ITS ALIVE!!!

--- src/test/java/getlandestate/pages/ControlPanelPage.java
+++ src/test/java/getlandestate/pages/ControlPanelPage.java
@@ -11,4 +11,34 @@ public class ControlPanelPage {
     }
     @FindBy(xpath = "(//span[@class='d-none d-sm-inline'])[3]")
     public WebElement categoriesButton;
+
+    @FindBy(xpath = "//button[contains(@class,'add-new-btn btn')]")
+    public WebElement categoriesAddNewButton;
+
+    @FindBy(id = "title")
+    public WebElement categoriesAddNewTitleBox;
+
+    @FindBy(id = "seq")
+    public WebElement categoriesAddNewSequenceBox;
+
+    @FindBy(id = "icon")
+    public WebElement categoriesAddNewIconBox;
+
+    @FindBy(xpath = "//button[contains(@class,'category-common-button create-button')]")
+    public WebElement categoriesAddNewCreateButton;
+
+    @FindBy(name = "search")
+    public WebElement categoriesSearchBox;
+
+    @FindBy(xpath = "//button[contains(@class,'search-button btn')]")
+    public WebElement categoriesSearchBoxSearchButton;
+
+    @FindBy(xpath = "//div[text()='OhBoyWhataUniqueName']")
+    public WebElement muratAssertionData;
+
+    @FindBy(xpath = "(//button[contains(@class,'btn-link btn')])[2]")
+    public WebElement categoriesFirstCategoryDeleteButton;
+
+    @FindBy(xpath = "(//span[@class='p-button-label p-c'])[2]")
+    public WebElement categoriesDeleteButtonConfirmButton;
 }
