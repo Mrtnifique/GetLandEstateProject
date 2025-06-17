2c9e726eec56b0f0a0370503ee659a263ac998cc
Emine <kemane2008@gmail.com>
2025-06-17 21:28:21 +0300

    eminev2

--- src/test/java/getlandestate/pages/AddsPage.java
+++ src/test/java/getlandestate/pages/AddsPage.java
@@ -10,21 +10,42 @@ public class AddsPage {
 
 
         PageFactory.initElements(Driver.getDriver(),this);
+
+        //ilanlarim butonu
 }
         @FindBy(xpath="//*[@id=\"subMenu\"]/div/a[2]/span[1]/span")
                 public WebElement addsBuuton;
+
+    //Ilan beklemede butonu
     @FindBy(xpath="(//*[@class='p-tag-value'])[1]")
                 public WebElement waitingButtonVerify;
+
+    //Degisim-guncelleme ana butonu
     @FindBy(xpath="(//*[@stroke-linejoin='round'])[3]")
                 public WebElement addsChangeButton;
+
+    //Silme butonu
     @FindBy(xpath="(//*[@stroke-linejoin='round'])[4]")
                 public WebElement addsDeleteButton;
+
+    //Aktiflestirme butonu
     @FindBy(xpath="//*[@id='custom-switch']")
                 public WebElement addsActivateButton; //(champs elysses)
+
+    //Guncelleme butonu
     @FindBy(xpath="(//*[@type='submit'])[1]")
                 public WebElement addsUpdateButton;
+
+    //Guncelleme Basarili Mesaji
     @FindBy(xpath="//button[contains(@class,'p-toast-detail')] ")
-                public WebElement addsUpdateVerifyButton;  //(kontrol edilecek )
+                public WebElement addsUpdateVerifyButton;//(kontrol edilecek )
+
+    //Ilan basariyla silindi mesaji
+    @FindBy(xpath=" ")
+                public WebElement addsDeletedVerify;
+
+
+
 
 
 }
