d677601f5dd828c591358d55bc2bc20cdc052a22
Mrtnifique <kilincarslanmrt@gmail.com>
2025-06-21 13:37:45 +0300

    Update DashBoardPageStepDefs.java

--- src/test/java/getlandestate/stepdefs/e2e_stepdefs/DashBoardPageStepDefs.java
+++ src/test/java/getlandestate/stepdefs/e2e_stepdefs/DashBoardPageStepDefs.java
@@ -13,6 +13,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 
 import java.util.Random;
