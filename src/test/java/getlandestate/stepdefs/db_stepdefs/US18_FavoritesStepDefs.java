b16132a5d6d52f34b8c8c4f8900b70d7e4f8d462
Mrtnifique <kilincarslanmrt@gmail.com>
2025-06-25 01:56:36 +0300

    Enough testing, action time

---
+++ src/test/java/getlandestate/stepdefs/db_stepdefs/US18_FavoritesStepDefs.java
@@ -0,0 +1,22 @@
+package getlandestate.stepdefs.db_stepdefs;
+
+import getlandestate.utilities.DBUtils;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+import java.util.List;
+
+public class US18_FavoritesStepDefs {
+    List<String> actualColumnData;
+
+    @Given("Kullanıcı {string} sekmesinden {string} id'den bilgileri çeker")
+    public void kullanıcıSekmesindenIdDenBilgileriÇeker(String sekme, String id) {
+
+    }
+
+    @Then("Kullanıcı gelen bilgileri doğrular")
+    public void kullanicigelenbilgileridogrular() {
+    }
+
+
+}
