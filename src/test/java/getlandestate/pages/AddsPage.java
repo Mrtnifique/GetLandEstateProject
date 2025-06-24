6c23bc31a9f9606d126369a512c306aabd60c75c
Mrtnifique <kilincarslanmrt@gmail.com>
2025-06-24 17:15:41 +0300

    Adds page

--- src/test/java/getlandestate/pages/AddsPage.java
+++ src/test/java/getlandestate/pages/AddsPage.java
@@ -7,11 +7,9 @@ import org.openqa.selenium.support.PageFactory;
 
 public class AddsPage {
     public AddsPage() {
-
-
         PageFactory.initElements(Driver.getDriver(),this);
-
         //ilanlarim butonu
+
 }
 
 
@@ -46,6 +44,8 @@ public class AddsPage {
   
   
   
+
+
 
 //Seval Aslan  baslangic 50
 
@@ -851,51 +851,51 @@ public class AddsPage {
     //Ilan basariyla silindi mesaji
     @FindBy(xpath=" ")
                 public WebElement addsDeletedVerify;
+    //Ilanlarim sayfasindaki ilanlar butonu
+    @FindBy(xpath = "( //span[contains(text(),'İlanlar')] ")
+    public WebElement addsOnAdverts;
 
 
 
+    //Ilanlarim sayfasindaki aratma kutucugu
+    @FindBy(xpath = "(//input[@id='q']  ")
+    public WebElement searchBoxOnAdverts;
 
 
 
+    //Ilanlarim sayfasindaki arama butonu
+    @FindBy(xpath = "( //button[@type='submit'] ")
+    public WebElement searchButtonOnAdverts;
 
 
 
+    //Ilanlarimdaki guncelleme butonu
+    @FindBy(xpath = "(  //tbody//button[2]//*[name()='svg']")
+    public WebElement updateButtonOnAdverts;
 
 
 
+    //Durum Aktif butonu
+    @FindBy(xpath = "( //span[@class='p-tag-value'] ")
+    public WebElement situationActiveButton;
 
 
+    //Durum Reddedildi butonu
+    @FindBy(xpath = "(//span[@class='p-tag-value']")
+    public WebElement statusRejectedButton;
+
+    //Guncelleme sayfasindaki Ddm butonu
+    @FindBy(xpath = "(//select[@id='statusForAdvert']")
+    public WebElement statusDdmButton;
+
+    // Guncelleme sayfasindaki update butonu
+    @FindBy(xpath = "(//button[@type='submit']")
+    public WebElement updateButtonOnEdit;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    // Guncelleme basarili mesaji
+    @FindBy(xpath = "(//body/div[@id='root']/div[@class='p-toast p-component p-toast-top-right p-ripple-disabled']/div/div[1]/div[1]/div[1]/div[1]")
+    public WebElement updateSuccesfulMessage;
 
     //Emine Arı bitis 900
 
