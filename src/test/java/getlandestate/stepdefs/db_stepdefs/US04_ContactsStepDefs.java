136b0469ebed11aac964d90873968b84e5845676
rezzan erva <ervaodabasi.07@gmail.com>
2025-06-27 17:31:03 +0300

    R10

--- src/test/java/getlandestate/stepdefs/db_stepdefs/US04_ContactsStepDefs.java
+++ src/test/java/getlandestate/stepdefs/db_stepdefs/US04_ContactsStepDefs.java
@@ -1,5 +1,41 @@
 package getlandestate.stepdefs.db_stepdefs;
 
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import java.util.List;
+
 public class US04_ContactsStepDefs {
 
+    List<String> actualColumnNames;
+
+    @When("Contacts tablosuna erisim saglanir")
+    public void contactsTablosunaErisimSaglanir() {
+
+    }
+
+    @Then("Headerlar dogrulanir")
+    public void headerlarDogrulanir() {
+        
+    }
+
+    @And("{int} numarali satir sorgulanir")
+    public void numaraliSatirSorgulanir(int arg0) {
+        
+    }
+
+    @And("Id degeri {int} olmalidir")
+    public void idDegeriOlmalidir(int arg0) {
+        
+    }
+
+    @And("First name fatma olmalidir")
+    public void firstNameFatmaOlmalidir() {
+        
+    }
+
+    @Then("Message kutusunda merhaba hayat yazmalidir")
+    public void messageKutusundaMerhabaHayatYazmalidir() {
+    }
 }
