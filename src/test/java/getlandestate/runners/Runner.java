faeb8576bce7872bbbda2346692a3ae351ed8c8a
Mrtnifique <kilincarslanmrt@gmail.com>
2025-06-27 19:45:03 +0300

    API Framework

--- src/test/java/getlandestate/runners/Runner.java
+++ src/test/java/getlandestate/runners/Runner.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = {"getlandestate/stepdefs", "getlandestate/hooks"},
 
-        tags = "@DBUS18",
+        tags = "",
 
 
 
