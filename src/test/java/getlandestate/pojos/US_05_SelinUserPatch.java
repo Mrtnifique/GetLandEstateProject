6b0230c1200f2a6a1f73ef9af8bb67e52046e24a
Mrtnifique <kilincarslanmrt@gmail.com>
2025-07-01 00:45:25 +0300

    Selin Hanım Final

--- src/test/java/getlandestate/pojos/US_05_SelinUserPatch.java
+++ src/test/java/getlandestate/pojos/US_05_SelinUserPatch.java
@@ -1,46 +1,70 @@
 package getlandestate.pojos;
 
 public class US_05_SelinUserPatch {
-	private String firstName;
-	private String lastName;
-	private String password;
-	private String role;
-	private String phone;
-	private String email;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String role;
+    private String phone;
+    private String email;
 
-	public US_05_SelinUserPatch() {
-	}
+    public US_05_SelinUserPatch() {
+    }
 
-	public US_05_SelinUserPatch(String firstName, String lastName, String password, String role, String phone, String email) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.password = password;
-		this.role = role;
-		this.phone = phone;
-		this.email = email;
-	}
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-	public String getFirstName(){
-		return firstName;
-	}
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-	public String getLastName(){
-		return lastName;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	public String getPassword(){
-		return password;
-	}
+    public void setRole(String role) {
+        this.role = role;
+    }
 
-	public String getRole(){
-		return role;
-	}
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 
-	public String getPhone(){
-		return phone;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public String getEmail(){
-		return email;
-	}
+    public US_05_SelinUserPatch(String firstName, String lastName, String password, String role, String phone, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.role = role;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
